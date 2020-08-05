063aa39b0c465d5c0bdeab02464c64d963d0a35b
AudieRaymos <68800496+AudieRaymos@users.noreply.github.com>
2020-08-05 15:51:59 -0700

    started js

---
+++ app.js
@@ -0,0 +1,13 @@
+//Selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoInput = document.querySelector('.todo-button');
+const todoInput = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+//Functions
+
+function addTodo(event){
+    console.log('hello');
+}
