3dbef35371716ea57e1faf831ecee5ea2ccc9c88
AudieRaymos <68800496+AudieRaymos@users.noreply.github.com>
2020-08-05 18:25:43 -0700

    Fixed errors is js

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 //Selectors 
 const todoInput = document.querySelector('.todo-input');
-const todoInput = document.querySelector('.todo-button');
-const todoInput = document.querySelector('.todo-list');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
-todoButton.addEventListener('click', addTodo)
+todoButton.addEventListener('click', addTodo);
 
 //Functions
 
 function addTodo(event) {
     //prevent form from submitting
-    event.preventdefault();
+    event.preventDefault();
     //Todo DIV 
     const todoDiv = document.createElement('div')
     todoDiv.classList.add('todo');
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.innertext = 'hey';
+    newTodo.innerText = 'hey';
     newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newtodo);
+    todoDiv.appendChild(newTodo);
     //CHECK MARK BUTTON
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa fa-check"></i>';
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
     //CHECK Trash BUTTON
-    const TrashButton = document.createElement('button');
-    TrashButton.innerHTML = '<i class="fas fa fa-trash"></i>';
-    TrashButton.classList.add("complete-btn");
-    todoDiv.appendChild(TrashButton);
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
 }
