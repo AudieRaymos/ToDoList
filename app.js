78994744defb43a7bf35eecf7e3c421ad8d12455
AudieRaymos <68800496+AudieRaymos@users.noreply.github.com>
2020-08-05 17:27:33 -0700

    Added buttons to js

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ todoButton.addEventListener('click', addTodo)
 
 //Functions
 
-function addTodo(event){
-    console.log('hello');
+function addTodo(event) {
+    //prevent form from submitting
+    event.preventdefault();
+    //Todo DIV 
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innertext = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newtodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK Trash BUTTON
+    const TrashButton = document.createElement('button');
+    TrashButton.innerHTML = '<i class="fas fa fa-trash"></i>';
+    TrashButton.classList.add("complete-btn");
+    todoDiv.appendChild(TrashButton);
 }
